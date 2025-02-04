9ca67e8c6c102e07094ba872adb7a4056b9cbc06
Garrett Bogart <bogartgarrett@gmail.com>
2025-02-03 17:51:07 -0700

    tree traversal

---
+++ src/PreorderTraversal.ts
@@ -0,0 +1,34 @@
+import { TreeNode } from './DataStructures/TreeNode';
+function preorderTraversal(root: TreeNode | null): number[] {
+    if(root == null) return [];
+    let order:number[] = [];
+    function traverse(node: TreeNode| null){
+        if(node == null) return;
+        order.push(node.val)
+        if(node?.left){
+            traverse(node.left);
+        }
+        if(node?.right){
+            traverse(node.right)
+        }
+    }
+    traverse(root)
+    return order;
+};
+
+let root = new TreeNode(1);
+root.right = new TreeNode(2);
+root.right.left = new TreeNode(3);
+
+let root1 = new TreeNode(1)
+root1.left = new TreeNode(2)
+root1.left.left = new TreeNode(4)
+root1.left.right = new TreeNode(5)
+root1.left.right.left = new TreeNode(6)
+root1.left.right.right = new TreeNode(7)
+root1.right = new TreeNode(3);
+root1.right.right = new TreeNode(8)
+root1.right.right.left = new TreeNode(9)
+
+console.log(preorderTraversal(root))
+console.log(preorderTraversal(root1))
